30a03fbc42c2a5d737465cd8e51d28a749a85788
kazukittin <kazutaku8492@gmail.com>
2026-06-22 20:52:19 +0900

    feat: configure Supabase project URL and anonymous API key

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.MY_DIET_CONFIG = {
-  supabaseUrl: "",
-  supabaseAnonKey: "",
+  supabaseUrl: "https://xyiuiqmkhqhtuepmneds.supabase.co",
+  supabaseAnonKey: "sb_publishable_oj_SFJ3N5JhSe3KwqBEOog_C4SkVt-8",
 };
